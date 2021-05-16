9f2a91fc25da990d698f6ae8caeda48dde8f47b7
Aleksandr <almax1984@gmail.com>
2021-05-16 22:22:44 +0500

    Practic html

--- js/script.js
+++ js/script.js
@@ -21,6 +21,38 @@ const movieDB = {
         "Ла-ла лэнд",
         "Одержимость",
         "Скотт Пилигрим против..."
-    ]
+    ]    
 };
+
+const adv = document.querySelectorAll('.promo__adv img'),
+      advTitle = document.querySelector('.promo__adv-title'),
+      poster = document.querySelector('.promo__bg'),
+      genre = poster.querySelector('.promo__genre'),
+      movieList = document.querySelector('.promo__interactive-list');
+
+adv.forEach(item => {
+    item.remove();
+});
+advTitle.remove();
+
+genre.textContent = 'Драмма';
+
+poster.style.backgroundImage = 'url("../img/bg.jpg")';
+
+movieList.innerHTML = "";
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+        <li class="promo__interactive-item">${i+1} ${film}
+            <div class="delete"></div>
+        </li>
+    `;
+});
+
+
+
+
+
 
