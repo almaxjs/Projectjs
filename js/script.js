2c20ab353793b42a34f5d57aa2258076ceee725a
Aleksandr <almax1984@gmail.com>
2021-05-26 15:07:04 +0500

    json-server

--- js/script.js
+++ js/script.js
@@ -293,5 +293,8 @@ window.addEventListener('DOMContentLoaded', () => {
         }, 40000);
     }
 
+    fetch('http://localhost:3000/menu')
+        .then(data => data.json())
+        .then(res => console.log(res));
 });
 
